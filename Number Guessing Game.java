15f19fa6f32c541886290c00aaac4d197310c9c0
shekinahsherfya09 <shekinahsherfya09@gmail.com>
2025-06-10 17:42:40 +0530

    Create Number Guessing Game.java

---
+++ Number Guessing Game.java
@@ -0,0 +1,27 @@
+import random
+
+def number_guessing_game():
+   
+    secret_number = random.randint(1, 100)
+    attempts = 0
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+
+    while True:
+        try:
+           
+            guess = int(input("Enter your guess: "))
+            attempts += 1
+
+            if guess < secret_number:
+                print("Too low. Try again.")
+            elif guess > secret_number:
+                print("Too high. Try again.")
+            else:
+                print(f"Congratulations! You guessed the number in {attempts} attempts.")
+                break
+
+        except ValueError:
+            print("Invalid input. Please enter a numeric value.")
+
+number_guessing_game()
